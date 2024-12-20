6e63f3a6d5c76d77c2950d442532cbe4a22c13c0
bkhebert <bondman2004tippy@gmail.com>
2024-12-20 16:47:31 -0600

    ADD google strategy, routes,  and schemas for login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const passport = require('passport')
 const router = express.Router();
 const GoogleStrategy = require('passport-google-oidc');
 var db = require('../db');
+const Federated_Credentials = require('../db/index.js');
 
 
 passport.use(new GoogleStrategy({
@@ -11,40 +12,52 @@ passport.use(new GoogleStrategy({
   callbackURL: '/oauth2/redirect/google',
   scope: [ 'profile' ]
 }, function verify(issuer, profile, cb) {
-  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    issuer,
-    profile.id
-  ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) {
-      db.run('INSERT INTO users (name) VALUES (?)', [
-        profile.displayName
-      ], function(err) {
-        if (err) { return cb(err); }
-        
-        var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-          id,
-          issuer,
-          profile.id
-        ], function(err) {
-          if (err) { return cb(err); }
-          var user = {
-            id: id,
-            name: profile.displayName
-          };
-          return cb(null, user);
-        });
-      });
-    } else {
-      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-        if (err) { return cb(err); }
-        if (!row) { return cb(null, false); }
-        return cb(null, row);
-      });
-    }
+  Federated_Credentials.findOneAndUpdate(
+    { 
+      provider: issuer, 
+      subject: profile._id 
+    }, 
+    { 
+      user_id: id , 
+      provider: issuer , 
+      subject: profile.displayName
+     },
+    {
+      upsert: true,
+      new: true,
+    })
+  .then((matchingCredentials) => {
+    console.log(matchingCredentials);
+  }).catch((err) => {
+    console.error(err);
+  })
+}))
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
   });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/login'
 }));
+
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+
 
 router.get('/login', (req, res, next) => {
   res.render('login');
