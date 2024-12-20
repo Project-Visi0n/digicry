2e0367219a256672e2c5c5642eede0b26a186edf
atheriot827 <atheriot827@gmail.com>
2024-12-20 15:11:53 -0600

    ADD React and Material UI dependancies

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+// Import necessary modules from React and Material UI
+
+
+
+// Define the App component
+const App = () => {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // Render the homepage
+  return (
+  
+  );
+};
+
+// Export the App component
+export default App;
