63ddb5a4f5c51cdc72e02f43e633e75c2fd18549
atheriot827 <atheriot827@gmail.com>
2024-12-20 20:19:15 -0600

    UPDATE container

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,29 +9,30 @@ const App = () => {
 
   // Render the homepage
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" className="glass-container">
       {/* Header Section */}
-      <Box my={4}>
-        <Typography variant="h3" component="h1" gutterBottom>
+        <Typography variant="h3" align="center" gutterBottom>
           Digi-Cry Today?
         </Typography>
+
+      {/* Future Sections: Mood Analytics and Local Events */}
+      <Box>
         <Typography variant="h6" component="p">
           Track your mood and connect with your community.
         </Typography>
+        {/* TODO: Add charts or graphs here for mood tracking */}
+      </Box>
+  
+      {/* Events Section */}
+      <Box mt={4}>
+        <Typography variant="h6">Local Events Near You:</Typography>
+        {/* TODO: Add local events here */}
       </Box>
   
       {/* Motivational Quote Section */}
       <Box>
-  
-      </Box>
-  
-      {/* Future Sections: Mood Analytics and Local Events */}
-      <Box>
-        {/* Placeholder for Mood Analytics */}
-      </Box>
-  
-      <Box>
-        {/* Placeholder for Local Events */}
+        <Typography variant="h6">Motivational Quote:</Typography>
+        {/* TODO: Add motivational quotes here */}
       </Box>
     </Container>
   );
