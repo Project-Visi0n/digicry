a81dcacf2aeca53d9d228944aa2e939baf58ca2b
atheriot827 <atheriot827@gmail.com>
2024-12-20 22:45:29 -0600

    FIX App.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
+// Import styles
+import './styles.css';
+
 //create Root file that will append to html element attribute with id:root
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>); 
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(<App />);
