65457fd5a598a3d0bee1521532a41a6c905580d1
atheriot827 <atheriot827@gmail.com>
2024-12-21 14:38:51 -0600

    add functionality to Route paths

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,6 +10,38 @@ import Login from './Login';
 const App = () => {
   return (
     <>
+    <Routes>
+      <Route path="/login" element={
+        !isAuthticated ? <Login /> : <Navigate to="/" replace />
+      } />
+
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Layout>
+            <Home />
+          </Layout>
+        </ProtectedRoute>
+      } />
+
+      <Route path="/journal" element={
+        <ProtectedRoute>
+          <Layout>
+            <Journal />
+          </Layout>
+        </ProtectedRoute>
+      } />
+
+      <Route path="/analytics" element={
+        <ProtectedRoute>
+          <Layout>
+            <Analytics />
+          </Layout>
+        </ProtectedRoute>
+      } />
+
+    </Routes>
     </>
   );
 };
+
+export default App;
