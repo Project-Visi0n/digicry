a259a84258a4d6bd55cdf2cfb9e8f07c8bfee8a9
atheriot827 <atheriot827@gmail.com>
2024-12-21 14:31:55 -0600

    add imports

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,10 @@
-
-
-
-
-
-
-
-
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from './Layout';
+import Home from './pages/Home';
+import Journal from './pages/Journal';
+import Analytics from './pages/Analytics';
+import Login from './Login';
 
 
 const App = () => {
