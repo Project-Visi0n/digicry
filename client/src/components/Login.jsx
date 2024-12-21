c31663d74ea3edd60adada411c2ac9df56551622
bkhebert <bondman2004tippy@gmail.com>
2024-12-21 16:59:01 -0600

    login component updated to send request on click

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function Login({ }) {
 
+  const handleClick = async() => {
+
+    axios.get('auth/google')
+  }
+
   return (
     <div>
     <h1>Sign in</h1>
-    <a class="button google" href="/auth/google">
+    <a class="button google" onClick={handleClick} href="auth/google">
     Sign in with Google
     </a>
     </div>
