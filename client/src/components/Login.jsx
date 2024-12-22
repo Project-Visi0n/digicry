78c5e5b7d8611c71028f4f85e03c32450435d2ec
bkhebert <bondman2004tippy@gmail.com>
2024-12-21 20:05:31 -0600

    ADD state properties and handleClick

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import { React, useState, useNavigate } from "react";
 
 function Login() {
+  const [login, setLogin] = useState(false);
+  const [href, setHref] = useState(`http://localhost:5000/auth/google`);
+  const [inOut, setInOut] = useState(`in`);
+ 
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (!login) {
+      setLogin(true);
+      setInOut("out");
+      // setHref("/");
+      
+    } else {
+      setLogin(false);
+      setInOut("in");
+      setHref("http://localhost:5000/auth/google");
+    }
+  };
 
   return (
     <div>
-    <h1>Sign in</h1>
-    <a className="button google" href="http://localhost:5000/auth/google">
-    Sign in with Google
-    </a>
+      <h1>Sign {inOut} </h1>
+      <a className="button google" href={href}  >
+        Sign {inOut} with Google
+      </a>
     </div>
-  )
+  );
 }
 
 export default Login;
