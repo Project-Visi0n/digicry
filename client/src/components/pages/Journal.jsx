25c9b092a426fff44ee87283d7110722a47e848d
atheriot827 <atheriot827@gmail.com>
2024-12-23 00:48:45 -0600

    feat: add imports

---
+++ client/src/components/pages/Journal.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  Textfield,
+  Stack,
+  IconButton,
+  Card,
+  CardContent,
+  CardActions,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+
