982e9bbcb530e172d9684a5202a5f1f0d55c7404
atheriot827 <atheriot827@gmail.com>
2024-12-26 20:30:53 -0600

    make homepage accessible without Login to continue development

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Login() {
-
   return (
     <div>
-    <h1>Sign in</h1>
-    <a className="button google" href="http://localhost:5001/auth/google">
-    Sign in with Google
-    </a>
+      <h1>Sign in</h1>
+      <a className="button google" href="http://localhost:5001/auth/google">
+        Sign in with Google
+      </a>
     </div>
-  )
+  );
 }
 
 export default Login;
