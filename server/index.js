1b8025e0335afa25e1949cd4cacae920e70ef6bc
coltongraygg <coltongraygg@gmail.com>
2025-01-01 10:26:59 -0600

    ADD geolocation route

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ const { User } = require("./models");
 // Import Routes
 const journalRoutes = require("./routes/journal");
 const eventRoutes = require("./routes/event");
+const geolocationRoutes = require("./routes/event");
 
 const PORT = process.env.PORT || 5000;
 
@@ -122,6 +123,7 @@ app.use("/api/journal", journalRoutes);
 
 // Events Route
 app.use("/api/events", eventRoutes);
+app.use("/api/geolocate", geolocationRoutes);
 
 // Log in with google route
 app.get(
