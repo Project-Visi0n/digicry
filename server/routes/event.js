03bc1d8f26f038bb3a5971ebc89b662ef389aa65
atheriot827 <atheriot827@gmail.com>
2025-01-02 18:31:01 -0600

    Fix: ESLint errors

--- server/routes/event.js
+++ server/routes/event.js
@@ -7,27 +7,26 @@ const { Event } = require("../models");
 /** NOTE !! THIS IS NOT DUPLICATE SAFE! ... YET! */
 /** LOCATION IS CURRENTLY HARD CODED */
 
-
-
 // GOOGLE MAPS API REVERSE GEOLOCATION
 // TAKES THE USERS LAT AND LONG AND GETS THEIR CITY
-router.get('/location', (req, res) => {
+router.get("/location", (req, res) => {
   // get users lat and long values from query
   const { latlng } = req.query;
   // api call to google maps
-  axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-    params: {
-      latlng: latlng,
-      key: process.env.GOOGLE_MAPS_API_KEY
-    }
-  })
+  axios
+    .get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        latlng,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    })
     .then((response) => {
       // assign variable to an array of objects where each obj has same keys
-        // ex: { long_name: 'Saint Andrew Street', short_name: 'St Andrew St', types: ['route'] }
+      // ex: { long_name: 'Saint Andrew Street', short_name: 'St Andrew St', types: ['route'] }
       // the obj with the city name has 'locality' value in types array
       // we find the right city name by finding the obj that has the 'locality' value in types
       const locInfo = response.data.results[0].address_components;
-      const city = locInfo.find(obj => obj.types.includes('locality'));
+      const city = locInfo.find((obj) => obj.types.includes("locality"));
 
       // add the city to the req object to use in SERP API call
       req.userLocation = city.long_name;
@@ -35,18 +34,18 @@ router.get('/location', (req, res) => {
       res.json(city.long_name); // in this object, long_name is the city name
     })
     .catch((err) => {
-      console.error('error with reverse geolocation', err);
+      console.error("error with reverse geolocation", err);
       res.status(500);
     });
 });
 
-
 // GET request to fetch event data from SERPAPI and save to our DB Event model
 router.get("/events", (req, res) => {
   const { userLocation } = req.query;
-  axios.get("https://serpapi.com/search", {
+  axios
+    .get("https://serpapi.com/search", {
       params: {
-        q: 'events',
+        q: "events",
         location: userLocation,
         engine: "google_events",
         api_key: process.env.EVENTS_API_KEY,
@@ -61,7 +60,7 @@ router.get("/events", (req, res) => {
         date: event.date.when,
         location: event.address,
         description: event.description,
-        venueName: event.venue && event.venue.name || 'N/A',
+        venueName: (event.venue && event.venue.name) || "N/A",
         linkUrl: event.link,
         ticketUrl: event.ticket_info[0].link,
         thumbnail: event.thumbnail,
