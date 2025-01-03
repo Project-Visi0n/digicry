00c6f4ff07c75f8fada7ac62c01c7723b39a0c52
atheriot827 <atheriot827@gmail.com>
2025-01-03 17:43:56 -0600

    feat: Create a detailed MoodAnalytics page

---
+++ client/src/components/pages/MoodAnalytics.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { Container, Typography, Box, Grid, Card, CardContent } from '@mui/material';
+import { Line, Bar } from 'react-chartjs-2';
+import axios from 'axios';
+
+function MoodAnalytics() {
+
+
+
+
+
+
+  
+  return (
+<>
+</>
+  )
+}
+
+export default MoodAnalytics;
