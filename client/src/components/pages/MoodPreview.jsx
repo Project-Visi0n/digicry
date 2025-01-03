44f8a9de88dd19576c64cde976719d3637ecfa98
atheriot827 <atheriot827@gmail.com>
2025-01-03 17:40:37 -0600

    feat: Create a visualization component for the mood preview on the homepage

---
+++ client/src/components/pages/MoodPreview.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Register ChartJS components
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+function MoodPreview() {
+  return <></>;
+}
+
+export default MoodPreview;
