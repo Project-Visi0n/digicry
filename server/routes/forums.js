725b12b7adb24b41b57b4350088195b82b53dc74
bkhebert <bondman2004tippy@gmail.com>
2025-01-04 16:15:57 -0600

    ADD decoy post if there are no posts

--- server/routes/forums.js
+++ server/routes/forums.js
@@ -6,7 +6,7 @@ const { Forums } = require("../models");
 
 // Creates a post and saves it to Forums model, sets an expiration date of 3 days
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   const { message, selectedGoal } = req.body;
   const noSpacesGoal = selectedGoal.split(" ").join("");
   const date = new Date();
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
   // Add a day
   const expiration = date.setDate(date.getDate() + 3);
   console.log(expiration)
-  await Forums.create({
+  Forums.create({
     forumName: noSpacesGoal,
     user: "anon",
     message,
@@ -35,17 +35,25 @@ router.post("/", async (req, res) => {
 
 // Gets all posts from forums based on query
 
-router.get("/", async (req, res) => {
+router.get("/", (req, res) => {
   
   const { query } = req;
   console.log('attempting to get the values of', query.forumName )
-  await Forums.find({ forumName: query.forumName })
+   Forums.find({ forumName: query.forumName })
     .then((posts) => {
       console.log('posts returned ', posts)
       if (posts.length > 0) {
         res.status(200).send(posts);
       } else {
-        res.status(404);
+        const fakeDate = new Date();
+        res.send([{
+          forumName: query.forumName,
+          user: 'anon',
+          message: "No posts have been made here yet!",
+          upVote: 0,
+          downVote: 0,
+          createdAt: fakeDate.getDate()
+        }])
       }
     })
     .catch((error) => {
